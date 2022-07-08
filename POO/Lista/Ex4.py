d8b97ff736113a7dd6be21a487ca6c456ccfdec1
Bhsenna <57589013+Bhsenna@users.noreply.github.com>
2022-07-08 15:27:08 -0300

    Aula de POO

--- POO/Lista/Ex4.py
+++ POO/Lista/Ex4.py
@@ -1,18 +1,18 @@
 class BombaCombustivel:
-    def __init__(self, tipo_combustivel: str, valor_litro: float, quantidade_combustivel: float):
+    def __init__(self, tipo_combustivel: str, valor_litro: float):
         self.tipo = tipo_combustivel
         self.valorLitro = valor_litro
-        self.quantidade = quantidade_combustivel
+        self.quantidade = 11000
 
     def abastecer_por_valor(self, valor: float):
         quantidade = valor / self.valorLitro
         print(f'Foi adicionado {quantidade:.2f} litros de gasolina ao tanque!')
-        self.quantidade += quantidade
+        self.quantidade -= quantidade
 
     def abastecer_por_litro(self, quantidade: float):
         preco = quantidade * self.valorLitro
         print(f'Você deve pagar R${preco:.2f} pelos {quantidade} litros!')
-        self.quantidade += quantidade
+        self.quantidade -= quantidade
 
     def alterar_valor(self, valor: float):
         print(f'Valor alterado com sucesso, de RS{self.valorLitro:.2f} para R${valor:.2f}')
@@ -23,11 +23,11 @@ class BombaCombustivel:
         self.tipo = novo_combustivel
 
     def alterar_quantidade_combustivel(self, nova_quantidade):
-        print(f'Quantidade de Combustível alterada com sucesso para {nova_quantidade} litro')
+        print(f'Quantidade de Combustível alterada com sucesso para {nova_quantidade} litros')
         self.quantidade = nova_quantidade
 
 
-b1 = BombaCombustivel('Etanol', 5.99, 10)
+b1 = BombaCombustivel('Etanol', 5.99)
 b1.abastecer_por_valor(50)
 b1.abastecer_por_litro(5)
 b1.alterar_valor(3.50)
