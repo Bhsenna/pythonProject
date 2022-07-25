501af474c5311d18d767544e6b423ff191fe5e6e
Hazime <bhsenna@gmail.com>
2022-07-25 16:22:50 -0300

    Tarefa Voos - Finalização da parte Interativa 1

--- banco de dados/Tarefa Ferias/Tarefa.py
+++ banco de dados/Tarefa Ferias/Tarefa.py
@@ -7,9 +7,10 @@ from Funções.Lista.Func import getFloat
 import pandas as pd
 import barcodenumber
 import arrow
+from bs4 import BeautifulSoup
 
 pd.set_option('display.max_columns', 14)
-pd.set_option('display.width', 200)
+pd.set_option('display.width', 400)
 
 conexao = sqlite3.connect('Voos.db')
 cursor = conexao.cursor()
@@ -30,7 +31,7 @@ def listar_Aeronaves():
         data['   Limite Bagagem (Kg)'] += [aeronave[3]]
     data_frame = pd.DataFrame(data)
     if str(data_frame).startswith('Empty DataFrame'):
-        print('Não há vendas registradas')
+        print('Não há Aeronaves registradas')
     else:
         print(data_frame)
 
@@ -50,7 +51,7 @@ def listar_Empresas():
         data['   Sigla'] += [aeronave[3]]
     data_frame = pd.DataFrame(data)
     if str(data_frame).startswith('Empty DataFrame'):
-        print('Não há vendas registradas')
+        print('Não há Empresas registradas')
     else:
         print(data_frame)
 
@@ -76,13 +77,13 @@ def listar_Aeroportos():
         data['   Continente'] += [aeronave[6]]
     data_frame = pd.DataFrame(data)
     if str(data_frame).startswith('Empty DataFrame'):
-        print('Não há vendas registradas')
+        print('Não há Aeroportos registrados')
     else:
         print(data_frame)
 
 
 def listar_Voos():
-    cursor.execute('SELECT * FROM Aeroportos')
+    cursor.execute('SELECT * FROM Voos')
     data = {
         '   Código': [],
         '   Data/Saída': [],
@@ -98,23 +99,23 @@ def listar_Voos():
         '   Hora/Chegada': [],
         '   Natureza Voo': []
     }
-    for aeronave in cursor.fetchall():
-        data['   Código'] += [aeronave[0]]
-        data['   Data/Saída'] += [aeronave[1]]
-        data['   Hora/Saída'] += [aeronave[2]]
-        data['   Aeroporto Decolagem'] += [aeronave[3]]
-        data['   Aeroporto Destino'] += [aeronave[4]]
-        data['   Empresa'] += [aeronave[5]]
-        data['   N° Passageiros'] += [aeronave[6]]
-        data['   Assentos Disponíveis'] += [aeronave[7]]
-        data['   Carga Carregada (Kg)'] += [aeronave[8]]
-        data['   Aeronave'] += [aeronave[9]]
-        data['   Data/Chegada'] += [aeronave[10]]
-        data['   Hora/Chegada'] += [aeronave[11]]
-        data['   Natureza Voo'] += [aeronave[12]]
+    for voo in cursor.fetchall():
+        data['   Código'] += [voo[0]]
+        data['   Data/Saída'] += [voo[1]]
+        data['   Hora/Saída'] += [voo[2]]
+        data['   Aeroporto Decolagem'] += [voo[3]]
+        data['   Aeroporto Destino'] += [voo[4]]
+        data['   Empresa'] += [voo[5]]
+        data['   N° Passageiros'] += [voo[6]]
+        data['   Assentos Disponíveis'] += [voo[7]]
+        data['   Carga Carregada (Kg)'] += [voo[8]]
+        data['   Aeronave'] += [voo[9]]
+        data['   Data/Chegada'] += [voo[10]]
+        data['   Hora/Chegada'] += [voo[11]]
+        data['   Natureza Voo'] += [voo[12]]
     data_frame = pd.DataFrame(data)
     if str(data_frame).startswith('Empty DataFrame'):
-        print('Não há vendas registradas')
+        print('Não há Voos registrados')
     else:
         print(data_frame)
 
@@ -128,14 +129,14 @@ class Aeronaves:
 
     def cadastrar(self):
         try:
-            cursor.execute('INSERT INTO Aeronaves (cod_aeronave, modelo, assentos_disponiveis, limite_bagagem(kg))'
+            cursor.execute('INSERT INTO Aeronaves (cod_aeronave, modelo, assentos_disponiveis, limite_bagagem)'
                            'VALUES(?,?,?,?)',
                            (self.cod_aeronave, self.modelo, self.assentos_disponiveis, self.limite_bagagem))
         except sqlite3.IntegrityError:
-            print('Código de Aeronave já está cadastrado\n')
+            print('\33[1;31mERRO:\33[0;31m Código de Aeronave já está cadastrado\33[0m\n')
 
     def alterar(self):
-        opcoes = ["modelo", "assentos_disponiveis", "limite_bagagem(kg)"]
+        opcoes = ["modelo", "assentos_disponiveis", "limite_bagagem"]
         escolha = getInt(
             'Você deseja alterar:\n[0] Modelo da Aeronave\n[1] Número de assentos disponíveis\n[2] Limite de Bagagem(kg)')
         novo_valor = input('Informe o novo valor: ')
@@ -162,7 +163,7 @@ class Empresas:
                            'VALUES(?,?,?,?)',
                            (self.cod_empresa, self.nome_empresa, self.nacionalidade, self.sigla_empresa))
         except sqlite3.IntegrityError:
-            print('Código de Empresa já está cadastrado\n')
+            print('\33[1;31mERRO:\33[0;31m Código de Empresa já está cadastrado\33[0m\n')
 
     def alterar(self):
         opcoes = ["nome_empresa", "nacionalidade", "sigla_empresa"]
@@ -198,7 +199,7 @@ class Aeroportos:
                     self.cod_aeroporto, self.nome_aeroporto, self.sigla_aeroporto, self.cidade, self.estado, self.pais,
                     self.continente))
         except sqlite3.IntegrityError:
-            print('Código de Aeroporto já está cadastrado\n')
+            print('\33[1;31mERRO:\33[0;31m Código de Aeroporto já está cadastrado\33[0m\n')
 
     def alterar(self):
         opcoes = ["nome_aeroporto", "sigla_aeroporto", "cidade", "estado", "pais", "continente"]
@@ -237,20 +238,20 @@ class Voos:
 
     def cadastrar(self):
         try:
-            cursor.execute('INSERT INTO Aeroportos (cod_voo, data_saida, hora_saida, cod_aeroporto_decolagem, '
+            cursor.execute('INSERT INTO Voos (cod_voo, data_saida, hora_saida, cod_aeroporto_decolagem, '
                            'cod_aeroporto_destino, cod_empresa, num_passageiros, assentos_disponiveis, '
-                           'carga_carregada(kg), cod_aeronave, data_chegada, hora_chegada, natureza_voo)'
+                           'carga_carregada, cod_aeronave, data_chegada, hora_chegada, natureza_voo)'
                            'VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)',
                            (self.cod_voo, self.data_saida, self.hora_saida, self.cod_aeroporto_decolagem,
                             self.cod_aeroporto_destino, self.cod_empresa, self.num_passageiros,
                             self.assentos_disponiveis, self.carga_carregada, self.cod_aeronave,
                             self.data_chegada, self.hora_chegada, self.natureza_voo))
         except sqlite3.IntegrityError:
-            print('Código de Voo já está cadastrado\n')
+            print('\33[1;31mERRO:\33[0;31m Código de Voo já está cadastrado\33[0m\n')
 
     def alterar(self):
         opcoes = ["data_saida", "hora_saida", "cod_aeroporto_decolagem", "cod_aeroporto_destino",
-                  "cod_empresa", "num_passageiros", "assentos_disponiveis", "carga_carregada(kg)",
+                  "cod_empresa", "num_passageiros", "assentos_disponiveis", "carga_carregada",
                   "cod_aeronave", "data_chegada", "hora_chegada", "natureza_voo"]
         escolha = getInt(
             'Você deseja alterar:\n[0] Data de Saída\n[1] Hora de Saída\n[2] Aeroporto de Decolagem'
@@ -267,19 +268,127 @@ class Voos:
             print(f'Voo foi deletada')
 
 
-dic1 = {'1': Aeronaves, '2': Empresas, '3': Aeronaves, '4': Voos}
+dic1 = {'1': 'Aeronave', '2': 'Empresa', '3': 'Aeroporto', '4': 'Voo'}
 while True:
+    aeronaves = []
+    cursor.execute('SELECT cod_aeronave FROM Aeronaves')
+    for i in cursor.fetchall():
+        aeronaves.append(i[0])
+    aeroportos = []
+    cursor.execute('SELECT cod_aeroporto FROM Aeroportos')
+    for i in cursor.fetchall():
+        aeroportos.append(i[0])
+    empresas = []
+    cursor.execute('SELECT cod_empresa FROM Empresas_Aereas')
+    for i in cursor.fetchall():
+        empresas.append(i[0])
+    voos = []
+    cursor.execute('SELECT cod_voo FROM Voos')
+    for i in cursor.fetchall():
+        voos.append(i[0])
     acao = input('Você deseja:\n'
                  '[1] Interagir com Aeronaves\n'
                  '[2] Interagir com Empresas\n'
-                 '[3] Interagir com Aeronaves\n'
+                 '[3] Interagir com Aeroportos\n'
                  '[4] Interagir com Voos\n').strip()
     acao2 = input('Você deseja:\n'
                   f'[1] Cadastrar {dic1[acao]}\n'
                   f'[2] Editar {dic1[acao]}\n'
                   f'[3] Deletar {dic1[acao]}\n').strip()
-    if acao == '1':
-        pass
+    if acao == '1':  # Aeronave
+        if acao2 == '1':  # Cadastrar
+            codigo = input('Informe o código da Aeronave: ')
+            modelo = input('Informe o modelo da Aeronave: ')
+            assentos_disponiveis = getInt('Informe a quantidade de Assentos Disponíveis: ')
+            limite_bagagem = getFloat('Informe o Limite de Bagagem (em Kg): ')
+            Aeronaves(codigo, modelo, assentos_disponiveis, limite_bagagem).cadastrar()
+        elif acao2 == '2':  # Editar
+            listar_Aeronaves()
+            codigo = input('Informe o Código da Aeronave que vai ser alterada: ')
+            Aeronaves(codigo).alterar()
+        elif acao2 == '3':  # Deletar
+            listar_Aeronaves()
+            codigo = input('Informe o Código da Aeronave que vai ser excluida: ')
+            Aeronaves(codigo).excluir()
+        listar_Aeronaves()
+    elif acao == '2':  # Empresa
+        if acao2 == '1':  # Cadastrar
+            codigo = input('Informe o código da Empresa: ')
+            nome = input('Informe o nome da Empresa: ')
+            nacionalidade = input('Informe a nacionalidade da Empresa: ')
+            sigla = input('Informe a sigla da Empresa: ')
+            Empresas(codigo, nome, nacionalidade, sigla).cadastrar()
+        elif acao2 == '2':  # Editar
+            listar_Empresas()
+            codigo = input('Informe o Código da Empresa que vai ser alterada: ')
+            Empresas(codigo).alterar()
+        elif acao2 == '3':  # Deletar
+            listar_Empresas()
+            codigo = input('Informe o Código da Empresa que vai ser excluida: ')
+            Empresas(codigo).excluir()
+        listar_Empresas()
+    elif acao == '3':  # Aeroportos
+        if acao2 == '1':  # Cadastrar
+            codigo = input('Informe o código do Aeroporto: ')
+            nome = input('Informe o nome do Aeroporto: ')
+            sigla = input('Informe a sigla do Aeroporto: ')
+            cidade = input('Informe a cidade do Aeroporto: ')
+            estado = input('Informe o estado do Aeroporto: ')
+            pais = input('Informe o país do Aeroporto: ')
+            continente = input('Informe o continente do Aeroporto: ')
+            Aeroportos(codigo, nome, sigla, cidade, estado, pais, continente).cadastrar()
+        elif acao2 == '2':  # Editar
+            listar_Aeroportos()
+            codigo = input('Informe o Código do Aeroporto que vai ser alterado: ')
+            Aeroportos(codigo).alterar()
+        elif acao2 == '3':  # Deletar
+            listar_Aeroportos()
+            codigo = input('Informe o Código do Aeroporto que vai ser exluido: ')
+            Aeroportos(codigo).excluir()
+        listar_Aeroportos()
+    elif acao == '4':  # Voos
+        if acao2 == '1':  # Cadastrar
+            codigo = input('Informe o código do Voo: ')
+            data_saida = arrow.now().format('DD/MM/YYYY')
+            hora_saida = arrow.now().format('HH:mm')
+            codigo_aeroporto_decolagem = input('Informe o código do Aeroporto de Decolagem: ')
+            while codigo_aeroporto_decolagem not in aeroportos:
+                codigo_aeroporto_decolagem = input('Informe um código válido: ')
+            codigo_aeroporto_destino = input('Informe o código do Aeroporto de Destino: ')
+            while codigo_aeroporto_destino not in aeroportos:
+                codigo_aeroporto_destino = input('Informe um código válido: ')
+            codigo_empresa = input('Informe o código da Empresa: ')
+            while codigo_empresa not in empresas:
+                codigo_empresa = input('Informe um código válido: ')
+            codigo_aeronave = input('Informe o código da Aeronave: ')
+            while codigo_aeronave not in aeronaves:
+                codigo_aeronave = input('Informe um código válido: ')
+            assentos_max = cursor.execute('SELECT assentos_disponiveis FROM Aeronaves WHERE cod_aeronave=?', (codigo_aeronave,)).fetchall()[0][0]
+            num_passageiros = getInt('Informe o número de passageiros: ')
+            while num_passageiros > assentos_max:
+                print(f'O número da passageiros excede a quantidade máxima de assentos! Esse avião suporta apenas {assentos_max} passageiros')
+                num_passageiros = getInt('Informe o número de passageiros: ')
+            assentos_disponiveis = assentos_max - num_passageiros
+            limite = cursor.execute('SELECT limite_bagagem FROM Aeronaves WHERE cod_aeronave=?', (codigo_aeronave,)).fetchall()[0][0]
+            carga_max = limite * num_passageiros
+            carga_carregada = getFloat('Informe a carga total carregada pela Aeronave (em Kg): ')
+            while carga_carregada > carga_max:
+                print(f'O peso total excede o limite por passageiro! Cada passageiro só pode carregar {limite} kg, para um peso total de no máximo {carga_max} kg')
+                carga_carregada = getFloat('Informe a carga total carregada pela Aeronave (em Kg): ')
+            data_chegada = input('Informe a data de chegada (DD/MM/YYYY): ')
+            hora_chegada = input('Informe a hora de chegada (HH:mm): ')
+            natureza = input('Informe se o Voo é Nacional ou Internacional: ')
+            Voos(codigo, data_saida, hora_saida, codigo_aeroporto_decolagem, codigo_aeroporto_destino, codigo_empresa, num_passageiros,
+                 assentos_disponiveis, carga_carregada, codigo_aeronave, data_chegada, hora_chegada, natureza).cadastrar()
+        elif acao2 == '2':  # Editar
+            listar_Voos()
+            codigo = input('Informe o Código do Voo que vai ser alterado: ')
+            Voos(codigo).alterar()
+        elif acao2 == '3':  # Deletar
+            listar_Voos()
+            codigo = input('Informe o Código do Voo que vai ser excluido: ')
+            Voos(codigo).excluir()
+        listar_Voos()
     conexao.commit()
     if input('Deseja parar? [S/N]\n').lower().startswith('s'):
         break
