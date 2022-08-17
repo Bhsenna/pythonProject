7f36592ec207d32eb350f38890e126ae1b96a1ca
Hazime <bhsenna@gmail.com>
2022-08-17 20:42:07 -0300

    Lista sobre JavaScript: Exercícios 001-012

---
+++ HTML+CSS/javascript/Lista HTML/acoes.js
@@ -0,0 +1,23 @@
+function acao1(){
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += "<p>Clicou no primeiro botão</p>"
+}
+
+function acao2(){
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += "<p>Clicou no segundo botão</p>"
+}
+
+function acao3(){
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += "<p>Clicou no terceiro botão</p>"
+}
+
+function acao4(){
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += "<p>Clicou no quarto botão</p>"
+}
